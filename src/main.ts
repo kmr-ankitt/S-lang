bb0abcf376a3374c43867594af8340e830699540
kmr-ankitt <sahuankit.murhu@gmail.com>
2024-06-17 21:51:11 +0530

    ambiguity resolved

--- src/main.ts
+++ src/main.ts
@@ -56,14 +56,4 @@ export default class Slang {
       console.log(token);
     }
   }
-
-  static error(line: number, message: string) {
-    this.report(line, "", message);
-  }
-
-  //   Error reporting is done here
-  private static report(line: number, where: string, message: string) {
-    console.error("[line " + line + "] Error" + where + ": " + message);
-    this.hadError = true;
-  }
 }
