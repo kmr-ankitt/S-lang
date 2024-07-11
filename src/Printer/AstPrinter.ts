53e6027f5dfbf828e64f5a1cf0b957d7e56d2ee8
Ankit kumar sahu <sahuankit.murhu@gmail.com>
2024-07-11 08:58:02 +0530

    Throwing Ast printer out

--- src/Printer/AstPrinter.ts
+++ src/Printer/AstPrinter.ts
@@ -1,30 +1,30 @@
-import { ExprVisitor, Expr, Binary, Grouping, Literal, Unary } from "../Ast/Expr";
-import { Token } from "../Tokens/token";
-import { TokenType } from "../Tokens/tokenType";
+// import { ExprVisitor, Expr, Binary, Grouping, Literal, Unary } from "../Ast/Expr";
+// import { Token } from "../Tokens/token";
+// import { TokenType } from "../Tokens/tokenType";
 
-export class AstPrinter implements ExprVisitor<string> {
-  print(expr: Expr) {
-    return expr.accept(this);
-  }
+// export class AstPrinter implements ExprVisitor<string> {
+//   print(expr: Expr) {
+//     return expr.accept(this);
+//   }
 
-  public visitBinaryExpr(expr: Binary): string {
-    return this.parenthesize(expr.operator.lexeme, expr.left, expr.right);
-  }
+//   public visitBinaryExpr(expr: Binary): string {
+//     return this.parenthesize(expr.operator.lexeme, expr.left, expr.right);
+//   }
 
-  public visitGroupingExpr(expr: Grouping): string {
-    return this.parenthesize("group", expr.expression);
-  }
+//   public visitGroupingExpr(expr: Grouping): string {
+//     return this.parenthesize("group", expr.expression);
+//   }
 
-  public visitLiteralExpr(expr: Literal): string {
-    if (expr.value == null) return "nil";
-    return expr.value.toString();
-  }
+//   public visitLiteralExpr(expr: Literal): string {
+//     if (expr.value == null) return "nil";
+//     return expr.value.toString();
+//   }
 
-  public visitUnaryExpr(expr: Unary): string {
-    return this.parenthesize(expr.operator.lexeme, expr.right);
-  }
+//   public visitUnaryExpr(expr: Unary): string {
+//     return this.parenthesize(expr.operator.lexeme, expr.right);
+//   }
 
-  private parenthesize(name: string, ...exprs: Expr[]): string {
-    return `(${name} ${exprs.map((expr) => `${expr.accept(this)}`).join(" ")})`;
-  }
-}
+//   private parenthesize(name: string, ...exprs: Expr[]): string {
+//     return `(${name} ${exprs.map((expr) => `${expr.accept(this)}`).join(" ")})`;
+//   }
+// }
