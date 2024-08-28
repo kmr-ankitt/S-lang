eb9c0e756830be3f0703d8d411db4475edc615f2
ankit <sahuankit.murhu@gmail.com>
2024-08-28 10:47:51 +0530

    S-lang final keyword

--- src/Lexer/mapping.ts
+++ src/Lexer/mapping.ts
@@ -1,20 +1,20 @@
 import { TokenType } from "../Tokens/tokenType";
 
 export const keywords: Record<string, TokenType> = {
-  and: TokenType.AND,
-  class: TokenType.CLASS,
-  else: TokenType.ELSE,
-  false: TokenType.FALSE,
-  for: TokenType.FOR,
-  fun: TokenType.FUN,
-  if: TokenType.IF,
-  nil: TokenType.NIL,
-  or: TokenType.OR,
-  print: TokenType.PRINT,
-  return: TokenType.RETURN,
-  super: TokenType.SUPER,
-  this: TokenType.THIS,
-  true: TokenType.TRUE,
-  var: TokenType.VAR,
-  while: TokenType.WHILE,
-};
+  n: TokenType.AND,            // "n" - short for "and"
+  crew: TokenType.CLASS,       // "crew" - a group, like a class
+  oof: TokenType.ELSE,         // "oof" - something went wrong, alternative path
+  noi: TokenType.FALSE,        // "noi" - clear denial
+  roll: TokenType.FOR,         // "roll" - rolling through a loop
+  fun: TokenType.FUN,          // "fun" - keeps the original but adds a playful tone
+  iffy: TokenType.IF,          // "iffy" - uncertain, conditional
+  void: TokenType.NIL,         // "void" - represents nothingness
+  or: TokenType.OR,            // "or" - one or the other
+  echo: TokenType.PRINT,       // "echo" - something that's repeated, like print
+  back: TokenType.RETURN,      // "back" - returning to something
+  top: TokenType.SUPER,        // "top" - something superior
+  here: TokenType.THIS,        // "here" - refers to the current context
+  real: TokenType.TRUE,        // "real" - simple affirmation
+  let: TokenType.VAR,          // "let" - a place to store things, like a variable
+  loop: TokenType.WHILE,       // "loop" - easy to relate to continuous repetition
+}
